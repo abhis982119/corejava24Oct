01a2a29079dbdc84978079f096f7458a2dd31d82
tf-sharma-abhishek <abhishek.sharma@tripfactory.com>
2024-10-26 20:10:39 +0530

    added

--- src/main/java/org/batch/classnobject/classvsobject/Runner.java
+++ src/main/java/org/batch/classnobject/classvsobject/Runner.java
@@ -1,8 +1,6 @@
 package org.batch.classnobject.classvsobject;
 
 public class Runner {
-
-
     public static void main(String[] args) {
         Student paramita = new Student(1, "Paramita");
         Student dikhsita = new Student(2, "Dikhsita");
